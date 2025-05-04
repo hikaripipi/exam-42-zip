334fe94bca982da11ec5c50b7a057b366976e3d5
hikaripipi <hikarishinagawa@cryptogames.co.jp>
2025-05-04 16:39:05 +0900

    practiced ft_strlen with a main

--- ex02/h_ft_strlen.c
+++ ex02/h_ft_strlen.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
 int	ft_strlen(char *str)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (str[i])
 	{
 		i++;
 	}
 	return (i);
+}
+
+int	main(void)
+{
+	char *str = "abcd";
+	printf("len:%d\n", ft_strlen(str));
 }
